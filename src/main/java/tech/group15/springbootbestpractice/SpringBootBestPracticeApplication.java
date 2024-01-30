865aaf2fc57ac01f63fa835fd1fa8fd39b4c3414
Bhishman <bhishman@dal.ca>
2024-01-30 18:45:37 -0400

    Spring boot starter steps

---
+++ src/main/java/tech/group15/springbootbestpractice/SpringBootBestPracticeApplication.java
@@ -0,0 +1,12 @@
+package tech.group15.springbootbestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBestPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootBestPracticeApplication.class, args);
+  }
+}
---
+++ src/test/java/tech/group15/springbootbestpractice/SpringBootBestPracticeApplicationTests.java
@@ -0,0 +1,11 @@
+package tech.group15.springbootbestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBestPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
